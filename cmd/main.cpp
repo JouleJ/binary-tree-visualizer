caa7b7543b775eef4388641a5bb98396f59d9307
gandarf <gandarfgamer200219@gmail.com>
2022-12-15 21:51:13 +0300

    Add flag parsing and const qualifiers to variables in common.hpp
    Add flag parsing, debug flag and help flag
    Add const/constexpr qualifiers to all variables in lib/common.hpp
    
    Signed-off-by: gandarf <gandarfgamer200219@gmail.com>

--- cmd/main.cpp
+++ cmd/main.cpp
@@ -1,16 +1,53 @@
 #include "gui/main-window.hpp"
 
+#include <iostream>
+#include <string>
+
 #include <QApplication>
 
+bool DEBUG = false;
+
+void parse_args(int argc, char **argv) {
+    for (size_t i = 1; i < argc; i++) {
+        std::string arg{argv[i]};
+        if (arg == "-d" || arg == "--debug") {
+            DEBUG = true;
+        } else
+        if (arg == "-h" || arg == "--help") {
+            std::cout << "Binary tree visualizer:\n"
+                         "(-d | --debug): debug mode\n"
+                         "(-h | --help): print this message and exit\n";
+            exit(0);
+        } else {
+            std::cout << "Unknown option. Print "
+                      << std::string(argv[0])
+                      << " -h for help\n";
+            exit(0);
+        }
+    }
+}
+
 int main(int argc, char **argv) {
-    const int64_t array[] = {-1, 2, 3, 4, 5, 6, 7};
-    auto segtree =
-        lib::MakeSegmentTree(&array[0], sizeof(array) / sizeof(array[0]));
+    parse_args(argc, argv);
+    if (!DEBUG) {
+        const auto app = QApplication(argc, argv);
 
-    const auto app = QApplication(argc, argv);
+        MainWindow mw;
+        mw.show();
+        return app.exec();
+    } else {
+        const int64_t array[] = {-1, 2, 3, 4, 5, 6, 7};
+        auto segtree =
+            lib::MakeSegmentTree(&array[0], sizeof(array) / sizeof(array[0]));
 
-    MainWindow mw;
-    mw.show();
+        const auto app = QApplication(argc, argv);
+        auto widget1 = DataStructureViewer(nullptr, segtree.get());
+        auto widget2 = Requester(nullptr, segtree.get());
+        QObject::connect(&widget2, &Requester::requestExecuted, &widget1,
+                        &DataStructureViewer::onRequestExecuted);
 
-    return app.exec();
+        widget1.show();
+        widget2.show();
+        return app.exec();
+    }
 }
--- lib/common.hpp
+++ lib/common.hpp
@@ -3,12 +3,12 @@
 #include <QColor>
 
 namespace common {
-inline auto GOLD = QColor(255, 215, 0);
-inline auto ORANGE = QColor(255, 140, 0);
+inline const auto GOLD = QColor(255, 215, 0);
+inline const auto ORANGE = QColor(255, 140, 0);
 
-inline auto defaultTextColor = Qt::black;
-inline auto defaultMetaColor = Qt::red;
+inline const auto defaultTextColor = Qt::black;
+inline const auto defaultMetaColor = Qt::red;
 
-inline size_t defaultAnimationDelay = 1000;
-inline size_t defaultSizeUnit = 16;
+inline constexpr size_t defaultAnimationDelay = 1000;
+inline constexpr size_t defaultSizeUnit = 16;
 } // namespace common
